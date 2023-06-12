263ee81724e78b774cbf2e9316218a0547878a62
Prates <guipratesdsantos@gmail.com>
2023-06-12 15:14:36 -0300

    calculadora-function-factory

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 function criaCalculadora() {
     return {
-        display: document.querySelector('.display')
+        display: document.querySelector('.display'),
 
         inicia() {
            this.botaoClicado() 
-        }
+        },
 
         botaoClicado() {
             document.addEventListener('click', e => {
-                let elemento = e.target
+                const elemento = e.target
+                
+                if(elemento.classList.contains('btn-numero')){
+                    this.botaoParaDisplay(elemento.innerText)
+                }
+                if(elemento.classList.contains('btn-limpar')){
+                    this.limparDisplay()
+                }
+                if(elemento.classList.contains('btn-delete')){
+                    this.deleteNumero()
+                }
             })
+        },
+
+        botaoParaDisplay(valor) {
+            this.display.value += valor
+        },
+
+        limparDisplay() {
+            this.display.value = ''
+        },
+
+        deleteNumero(){
+            this.display.value = this.display.value.slice(0, -1)
         }
     }
 }
